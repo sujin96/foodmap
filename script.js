32b06d38632bdd332247f8c0c783eb49444382e4
leesujin <jean0518@naver.com>
2022-07-12 19:48:28 +0900

    7/12 kakao map api setting

--- script.js
+++ script.js
@@ -1,8 +1,169 @@
-var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-var options = {
+// 1. 지도 생성 & 확대 축소 컨트롤러
+// 2. 더미데이터 준비하기 (제목, 주소, url, 카테고리)
+// 3. 여러개 마커 찍기
+//   * 주소 - 좌표 변환 (지도 라이브러리)
+// 4. 마커에 인포윈도우 붙이기
+//   * 마커에 클릭 이벤트로 인포윈도우
+//   * url에서 섬네일 따기
+//   * 클릭한 마커로 지도 센터 이동
+// 5. 카테고리 분류
+
+let container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+let options = {
   //지도를 생성할 때 필요한 기본 옵션
   center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-  level: 3, //지도의 레벨(확대, 축소 정도)
+  level: 8, //지도의 레벨(확대, 축소 정도)
 };
 
-var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+// 지도 확대 축소를 제어할 수 있는 줌 컨트롤을 생성합니다.
+let zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+/*
+**********************************************************
+2. 더미데이터 준비하기 (제목, 주소, url, 카테고리)
+*/
+const dataSet = [
+  {
+    title: "희락돈까스",
+    address: "서울 영등포구 양산로 210",
+    url: "https://www.youtube.com/watch?v=1YOJbOUR4vw&t=88s",
+    category: "양식",
+  },
+  {
+    title: "즉석우동짜장",
+    address: "서울 영등포구 대방천로 260",
+    url: "https://www.youtube.com/watch?v=1YOJbOUR4vw&t=88s",
+    category: "한식",
+  },
+  {
+    title: "아카사카",
+    address: "서울 서초구 서초대로74길 23",
+    url: "https://www.youtube.com/watch?v=1YOJbOUR4vw&t=88s",
+    category: "일식",
+  },
+];
+
+/*
+**********************************************************
+3. 여러개 마커 찍기
+  * 주소 - 좌표 변환
+https://apis.map.kakao.com/web/sample/multipleMarkerImage/ (여러개 마커)
+https://apis.map.kakao.com/web/sample/addr2coord/ (주소로 장소 표시하기)
+*/
+
+// 주소 - 좌표 변환 함수 (비동기 문제 발생 해결) ****************
+// 주소-좌표 변환 객체를 생성합니다
+var geocoder = new kakao.maps.services.Geocoder();
+function getCoordsByAddress(address) {
+  // promise 형태로 반환
+  return new Promise((resolve, reject) => {
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch(address, function (result, status) {
+      // 정상적으로 검색이 완료됐으면
+      if (status === kakao.maps.services.Status.OK) {
+        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+        return resolve(coords);
+      }
+      reject(new Error("getCoordsByAddress Error: not valid Address"));
+    });
+  });
+}
+
+/* 
+******************************************************************************
+4. 마커에 인포윈도우 붙이기
+  * 마커에 클릭 이벤트로 인포윈도우 https://apis.map.kakao.com/web/sample/multipleMarkerEvent/
+  * url에서 섬네일 따기
+  * 클릭한 마커로 지도 센터 이동 https://apis.map.kakao.com/web/sample/moveMap/
+*/
+
+async function setMap() {
+  for (var i = 0; i < dataSet.length; i++) {
+    let position = await getCoordsByAddress(dataSet[i].address);
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+      map: map, // 마커를 표시할 지도
+      position: position, // 마커를 표시할 위치
+    });
+
+    // 마커에 표시할 인포윈도우를 생성합니다
+    var infowindow = new kakao.maps.InfoWindow({
+      content: getContent(dataSet[i]), // 인포윈도우에 표시할 내용
+      disableAutoPan: true, // 인포윈도우를 열 때 지도가 자동으로 패닝하지 않을지의 여부 (기본값: false)
+    });
+
+    infowindowArray.push(infowindow);
+
+    // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+    // 이벤트 리스너로는 클로저를 만들어 등록합니다
+    // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+    kakao.maps.event.addListener(
+      marker,
+      "click",
+      makeOverListener(map, marker, infowindow, position)
+    );
+    // 커스텀: 맵을 클릭하면 현재 나타난 인포윈도우가 없어지게끔
+    kakao.maps.event.addListener(map, "click", makeOutListener(infowindow));
+  }
+}
+
+// 인포윈도우를 표시하는 클로저를 만드는 함수입니다
+/* 
+  커스텀
+  1. 클릭시 다른 인포윈도우 닫기
+  2. 클릭한 곳으로 지도 중심 이동하기
+  */
+
+function makeOverListener(map, marker, infowindow, position) {
+  return function () {
+    // 1. 클릭시 다른 인포윈도우 닫기
+    closeInfowindow();
+    infowindow.open(map, marker);
+    // 2. 클릭한 곳으로 짇 중심 이동하기
+    map.panTo(position);
+  };
+}
+
+// 커스텀
+// 1. 클릭시 다른 인포윈도우 닫기
+let infowindowArray = [];
+function closeInfowindow() {
+  for (let infowindow of infowindowArray) {
+    infowindow.close();
+  }
+}
+
+// 인포윈도우를 닫는 클로저를 만드는 함수입니다
+function makeOutListener(infowindow) {
+  return function () {
+    infowindow.close();
+  };
+}
+
+// HTML 코드로 바꾸는 함수
+function getContent(data) {
+  let videoId = "";
+  let replaceUrl = data.url;
+  replaceUrl = replaceUrl.replace("https://youtu.be/", "");
+  replaceUrl = replaceUrl.replace("https://www.youtube.com/embed/", "");
+  replaceUrl = replaceUrl.replace("https://www.youtube.com/watch?v=", "");
+  videoId = replaceUrl.split("&")[0];
+
+  const result = `<div class="infowindow">
+    <div class="infowindow-img-container">
+      <img src="https://img.youtube.com/vi/${videoId}/sddefault.jpg" class="infowindow-img" alt="...">
+    </div>
+    <div class="infowindow-body">
+      <h5 class="infowindow-title">${data.title}</h5>
+      <p class="infowindow-text">${data.address}</p>
+      <a href="https://youtu.be/${videoId}" target="_blank" class="infowindow-btn">영상이동</a>
+    </div>
+  </div>`;
+  return result;
+}
+
+setMap();
